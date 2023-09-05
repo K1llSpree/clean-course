f5bf9b41a601d3b7341912bd2625f0a6c8635d79
AJPerezMirex <47699912+K1llSpree@users.noreply.github.com>
2023-09-05 18:40:20 -0400

    Prueba

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
-import './style.css'
-
+import './style.css';
+import './clean-code/06-classes-a'
 const app = document.querySelector<HTMLDivElement>('#app')!
 
 app.innerHTML = `
   <h1>CleanCode y SOLID</h1>
   <span>Revisar la consola de JavaScript</span>
 `
+export interface IPerson{
+   id: string;
+   fullName: string;
+   fullLastName: string;
+}
+export class Person implements IPerson {
+  id: string;
+  fullName: string;
+  fullLastName: string;
 
+  constructor (id: string = '', fullName: string = '', fullLastName: string = ''){
+    this.id = id,
+    this.fullLastName = fullLastName,
+    this.fullName = fullName
+  }
+}
